26d6ab5e9108cc7eb027448eb4be307818792d3b
domnitskiy.oleg <o.domnitskiy@appomart.com>
2024-11-04 17:20:40 +0400

    Update ESLint configuration for React 18.3 and TypeScript

--- eslint.config.js
+++ eslint.config.js
@@ -5,9 +5,15 @@ import pluginReact from 'eslint-plugin-react'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  { settings: { react: { version: '18.3' } } },
   {
     languageOptions: {
       globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
   pluginJs.configs.recommended,
@@ -25,5 +31,8 @@ export default [
       'no-console': 'warn',
       'no-return-assign': 'off',
     },
+  },
+  {
+    ignores: ['eslint.config.js'],
   },
 ]
