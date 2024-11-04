b9f560f077e537119a853b39da95c7e00d58c161
domnitskiy.oleg <o.domnitskiy@appomart.com>
2024-11-04 17:20:58 +0400

    Update link attributes in App component JSX

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,12 @@ function App() {
   const test = 1
   console.log(1)
   return (
-    <>
+    <div>
       <div>
-        <a href='https://vite.dev' target='_blank' rel="noreferrer">
+        <a href='https://vite.dev' target='_blank' rel='noreferrer'>
           <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
-        <a href='https://react.dev' target='_blank' rel="noreferrer">
+        <a href='https://react.dev' target='_blank' rel='noreferrer'>
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
@@ -29,7 +29,7 @@ function App() {
       <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
