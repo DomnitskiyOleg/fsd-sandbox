aa1bc1177623d40ff64537d6dc3f5a36971855d9
domnitskiy.oleg <o.domnitskiy@appomart.com>
2024-11-12 12:54:35 +0400

    Refactor Header component for improved layout styling

--- src/app/Layout/components/Header.tsx
+++ src/app/Layout/components/Header.tsx
@@ -1,26 +1,41 @@
 import Stack from '@mui/material/Stack'
 import NavbarBreadcrumbs from './NavbarBreadcrumbs'
-
+import { Box, Container, Divider, Paper } from '@mui/material'
 
 export default function Header() {
   return (
-    <Stack
-      direction='row'
+    <Box
       sx={{
-        display: { xs: 'none', md: 'flex' },
-        width: '100%',
-        alignItems: { xs: 'flex-start', md: 'center' },
-        justifyContent: 'space-between',
-        maxWidth: { sm: '100%', md: '1700px' },
-        pt: 1.5,
-        backgroundColor: 'red',
+        display: { xs: 'none', md: 'block' },
+        pt: 1,
+        left: 280,
+        right: 0,
+        position: 'fixed',
+        zIndex: 999,
       }}
-      spacing={3}
     >
-      <NavbarBreadcrumbs />
-      <Stack direction='row' sx={{ gap: 1 }}>
-    
-      </Stack>
-    </Stack>
+      <Paper
+        sx={(theme) => ({
+          backgroundColor: '#CD5434',
+          borderRadius: 2,
+          border: '1px solid',
+          borderColor: theme.palette.grey[700],
+        })}
+      >
+        <Container maxWidth='xl'>
+          <Stack
+            direction='row'
+            sx={{
+              display: { xs: 'none', md: 'flex' },
+              height: 60,
+              alignItems: { xs: 'flex-start', md: 'center' },
+              justifyContent: 'space-between',
+            }}
+          >
+            <NavbarBreadcrumbs />
+          </Stack>
+        </Container>
+      </Paper>
+    </Box>
   )
 }
