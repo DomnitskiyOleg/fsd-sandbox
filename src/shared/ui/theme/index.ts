2adc7d679986b9e25117127e159b1ffe4ff03f26
domnitskiy.oleg <o.domnitskiy@appomart.com>
2024-11-12 12:53:01 +0400

    Update dark and light color schemes in theme settings

--- src/shared/ui/theme/index.ts
+++ src/shared/ui/theme/index.ts
@@ -2,7 +2,41 @@ import { createTheme } from '@mui/material/styles'
 
 export default createTheme({
   colorSchemes: {
-    dark: true,
-    light: true,
+    dark: {
+      palette: {
+        text: {
+          primary: '#ffff',
+        },
+        background: {
+          default: '#2E282A',
+          paper: '#2E282A',
+        },
+        primary: {
+          light: '#69BF8E',
+          main: '#259254',
+          dark: '#015D29',
+          contrastText: '#fff',
+        },
+        secondary: {
+          light: '#ff7961',
+          main: '#f44336',
+          dark: '#ba000d',
+          contrastText: '#000',
+        },
+      },
+    },
+    light: {
+      palette: {
+        text: {
+          primary: '#030101',
+        },
+        primary: {
+          light: '#69BF8E',
+          main: '#259254',
+          dark: '#015D29',
+          contrastText: '#fff',
+        },
+      },
+    },
   },
 })
