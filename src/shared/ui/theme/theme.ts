1aef44b07ceeb8c062d8c2cce78a3fa98b210db3
domnitskiy.oleg <o.domnitskiy@appomart.com>
2024-11-12 19:53:41 +0400

    Update theme file with new color scheme configurations

---
+++ src/shared/ui/theme/theme.ts
@@ -0,0 +1,52 @@
+import { createTheme } from '@mui/material/styles'
+
+export const THEME = createTheme({
+  colorSchemes: {
+    dark: {
+      palette: {
+        text: {
+          primary: '#FFF',
+        },
+        background: {
+          default: '#2E282A',
+          paper: '#2E282A',
+        },
+        primary: {
+          light: '#98DAB5',
+          main: '#69BF8E',
+          dark: '#43A46D',
+          contrastText: '#FFF',
+        },
+        secondary: {
+          light: '#F9A68E',
+          main: '#F56840',
+          dark: '#E9461C',
+          contrastText: '#000',
+        },
+      },
+    },
+    light: {
+      palette: {
+        background: {
+          default: '#FFFAF0',
+          paper: '#FFFAF0',
+        },
+        text: {
+          primary: '#030101',
+        },
+        primary: {
+          light: '#69BF8E',
+          main: '#0DA560',
+          dark: '#00844A',
+          contrastText: '#FFF',
+        },
+        secondary: {
+          light: '#FF7961',
+          main: '#B24B2E',
+          dark: '#902F13',
+          contrastText: '#000',
+        },
+      },
+    },
+  },
+})
