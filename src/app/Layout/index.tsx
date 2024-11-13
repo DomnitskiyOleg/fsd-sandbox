0f9fc74f7a492a9a9d66e8e77ade8e8dba3d3853
domnitskiy.oleg <o.domnitskiy@appomart.com>
2024-11-13 17:19:01 +0400

    Refactor layout component with new UI components

--- src/app/Layout/index.tsx
+++ src/app/Layout/index.tsx
@@ -1,41 +1,11 @@
-import { Box, Stack } from '@mui/material'
-import { Outlet } from 'react-router-dom'
-import ThemeTest from '../ThemeTest'
-import Header from './components/Header'
-import SideMenu from './components/SideMenu'
+import { LayoutMain } from '@/shared/ui'
+import { SideMenu } from '@/widgets/SideMenu'
+import { ContactCard } from '@/widgets/ContactCard'
+import { LayoutHeader } from '@/widgets/LayoutHeader'
 
-import AppNavbar from './components/AppNavbar'
-import CardAlert from './components/CardAlert'
-
-export default function Layout() {
-  return (
-    <Box sx={{ display: 'flex' }}>
-      <AppNavbar />
-      <SideMenu />
-      <Header />
-      {/* Main content */}
-      <Box
-        component='main'
-        sx={() => ({
-          flexGrow: 1,
-          pl: { xs: 0, md: '280px' },
-          minHeight: '100dvh',
-        })}
-      >
-        <Stack
-          sx={{
-            flexGrow: 1,
-            alignItems: 'center',
-            mt: { xs: 9, md: 10 },
-          }}
-        >
-          <Outlet />
-          <ThemeTest />
-          <CardAlert />
-          <CardAlert />
-        </Stack>
-      </Box>
-      {/* Main content */}
-    </Box>
-  )
-}
+export default (
+  <LayoutMain
+    headerSlot={LayoutHeader}
+    sideMenuSlot={<SideMenu contactCardSlot={<ContactCard />} />}
+  />
+)
