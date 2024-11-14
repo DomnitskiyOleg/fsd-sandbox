a8f0a7e1b1792b61f7a56001dd2fd12ec01e1367
domnitskiy.oleg <o.domnitskiy@appomart.com>
2024-11-14 12:27:26 +0400

    Refactor switch components to use Paper and customize style

--- src/features/language/changeLanguage/ui/ChangeLanguage.tsx
+++ src/features/language/changeLanguage/ui/ChangeLanguage.tsx
@@ -2,6 +2,7 @@ import { styled } from '@mui/material/styles'
 import Switch from '@mui/material/Switch'
 import getCssStyles from './styles'
 import { useTranslation } from 'react-i18next'
+import { Paper } from '@mui/material'
 
 const CustomSwitch = styled(Switch)(getCssStyles)
 
@@ -9,11 +10,15 @@ export function ChangeLanguage() {
   const { i18n } = useTranslation()
   const locale = i18n.language
   return (
-    <CustomSwitch
-      checked={locale === 'ru'}
-      onClick={() => {
-        void i18n.changeLanguage(locale === 'ru' ? 'en' : 'ru')
-      }}
-    />
+    <Paper
+      sx={{ backgroundColor: 'transparent', borderRadius: 4 }}
+    >
+      <CustomSwitch
+        checked={locale === 'ru'}
+        onClick={() => {
+          void i18n.changeLanguage(locale === 'ru' ? 'en' : 'ru')
+        }}
+      />
+    </Paper>
   )
 }
--- src/features/theme/changeTheme/ui/ChangeTheme.tsx
+++ src/features/theme/changeTheme/ui/ChangeTheme.tsx
@@ -1,17 +1,21 @@
 import { styled, useColorScheme } from '@mui/material/styles'
 import Switch from '@mui/material/Switch'
 import getCssStyles from './styles'
+import { Paper } from '@mui/material'
 
 const CustomSwitch = styled(Switch)(getCssStyles)
 
 export function ChangeTheme() {
   const { mode, setMode } = useColorScheme()
+
   return (
-    <CustomSwitch
-      checked={mode === 'dark'}
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-      }}
-    />
+    <Paper sx={{ backgroundColor: 'transparent', borderRadius: 4 }}>
+      <CustomSwitch
+        checked={mode === 'dark'}
+        onClick={() => {
+          setMode(mode === 'light' ? 'dark' : 'light')
+        }}
+      />
+    </Paper>
   )
 }
