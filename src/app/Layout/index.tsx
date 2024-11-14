c49096227ce94d9d448d9e1efedc0d120e53f236
domnitskiy.oleg <o.domnitskiy@appomart.com>
2024-11-14 19:44:23 +0400

    Delete deprecated components and add AppNavbar to Layout

--- src/app/Layout/index.tsx
+++ src/app/Layout/index.tsx
@@ -2,10 +2,12 @@ import { LayoutMain } from '@/shared/ui'
 import { SideMenu } from '@/widgets/SideMenu'
 import { ContactCard } from '@/widgets/ContactCard'
 import { LayoutHeader } from '@/widgets/LayoutHeader'
+import AppNavbar from './components/AppNavbar'
 
 export default (
   <LayoutMain
     headerSlot={LayoutHeader}
     sideMenuSlot={<SideMenu contactCardSlot={<ContactCard />} />}
+    appNavbar={<AppNavbar />}
   />
 )
