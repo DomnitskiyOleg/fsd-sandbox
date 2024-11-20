08843c9f2a96ff206644862b5a5d44d232fe5619
domnitskiy.oleg <o.domnitskiy@appomart.com>
2024-11-20 20:49:57 +0400

    Update not found page UI with new design and translations

--- src/pages/notFound/ui/Page.tsx
+++ src/pages/notFound/ui/Page.tsx
@@ -1,21 +1,52 @@
 import { Box, Button, Container, Typography } from '@mui/material'
-import NotFoundImage from './image.svg'
+
+import NewReleasesTwoToneIcon from '@mui/icons-material/NewReleasesTwoTone'
+import { useTranslation } from 'react-i18next'
+import { NavLink } from '@/shared/ui'
 
 export function NotFoundPage() {
+  const { t } = useTranslation('not-found')
   return (
-    <Container>
+    <Container
+      maxWidth='xl'
+      sx={{
+        flex: 1,
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
       <Box
+        flex={1}
         display='flex'
         flexDirection='column'
         justifyContent='center'
         alignItems='center'
       >
-        <Box component='img' src={NotFoundImage} />
-        <Typography variant='h3' textAlign='center'>
-          Oops! We can&apos;t seem to find the page you&lsquo;re looking for.
+        <NewReleasesTwoToneIcon
+          sx={(theme) => ({ color: theme.palette.primary.main, fontSize: 200 })}
+        />
+        <Typography
+          textTransform='uppercase'
+          sx={(theme) => ({
+            mt: theme.spacing(2),
+          })}
+          variant='h3'
+          textAlign='center'
+        >
+          {t('title')}
         </Typography>
-        <Typography textAlign='center'>125</Typography>
-        <Button>123</Button>
+        <Typography
+          sx={(theme) => ({
+            mt: theme.spacing(2),
+            mb: theme.spacing(2),
+          })}
+          textAlign='center'
+        >
+          {t('subtitle')}
+        </Typography>
+        <NavLink to='/'>
+          <Button variant='outlined'>{t('backButton')}</Button>
+        </NavLink>
       </Box>
     </Container>
   )
