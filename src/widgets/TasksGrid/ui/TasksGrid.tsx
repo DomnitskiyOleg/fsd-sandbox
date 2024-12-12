05b74557d8c933c0471f4ee1505238751ff18ec0
domnitskiy.oleg <o.domnitskiy@appomart.com>
2024-12-12 20:41:08 +0400

    Update import paths for task related features in TasksGrid

--- src/widgets/TasksGrid/ui/TasksGrid.tsx
+++ src/widgets/TasksGrid/ui/TasksGrid.tsx
@@ -1,10 +1,10 @@
 import { Container } from '@mui/material'
 import Grid from '@mui/material/Grid2'
 import { useSelector } from 'react-redux'
-import { selectTasks } from '@/app/Store/slices/tasksSlice'
 import { TaskCard } from '@/entities/task'
-
-
+import { selectTasks } from '@/entities/task/model/slice'
+import { DeleteTaskButton } from '@/features/task/delete'
+import { ChangeStatusButton } from '@/features/task/changeStatus'
 
 export function TasksGrid() {
   const tasks = useSelector(selectTasks)
@@ -14,7 +14,13 @@ export function TasksGrid() {
       <Grid sx={{ mt: 4 }} container spacing={2}>
         {tasks.map((v) => (
           <Grid key={v.id} size={{ xs: 12, sm: 6, lg: 4, xl: 3 }}>
-            <TaskCard task={v} />
+            <TaskCard
+              task={v}
+              deleteTaskSlot={<DeleteTaskButton id={v.id} />}
+              changeStatusSlot={
+                <ChangeStatusButton id={v.id} currentStatus={v.status} />
+              }
+            />
           </Grid>
         ))}
       </Grid>
