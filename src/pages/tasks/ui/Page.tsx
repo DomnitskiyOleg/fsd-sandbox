80bc0ac2007d77d4fe76816a53f06e162fbc034e
domnitskiy.oleg <o.domnitskiy@appomart.com>
2024-12-13 17:40:34 +0400

    Remove unused import TasksKanban from TasksPage

--- src/pages/tasks/ui/Page.tsx
+++ src/pages/tasks/ui/Page.tsx
@@ -1,6 +1,6 @@
 import { AddTaskForm } from '@/features/task/add'
 import { TasksGrid } from '@/widgets/TasksGrid'
-import { TasksKanban } from '@/widgets/TasksKanban'
+
 
 export function TasksPage() {
   return (
