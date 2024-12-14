ad4962edafad6a0dd42b6039e6a2ab818b2182ed
domnitskiy.oleg <o.domnitskiy@appomart.com>
2024-12-14 17:26:21 +0400

    Add TasksFilter to AddTaskForm in TasksPage

--- src/pages/tasks/ui/Page.tsx
+++ src/pages/tasks/ui/Page.tsx
@@ -1,11 +1,11 @@
 import { AddTaskForm } from '@/features/task/add'
+import { TasksFilter } from '@/features/task/filter'
 import { TasksGrid } from '@/widgets/TasksGrid'
-
 
 export function TasksPage() {
   return (
     <>
-      <AddTaskForm />
+      <AddTaskForm filterSlot={<TasksFilter />} />
       <TasksGrid />
     </>
   )
