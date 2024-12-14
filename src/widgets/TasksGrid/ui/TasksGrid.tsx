c47755ad710df8e6882b1dc92c3babd01b3c7433
domnitskiy.oleg <o.domnitskiy@appomart.com>
2024-12-14 20:27:06 +0400

    Update TasksGrid to conditionally render MobileTasksFilter

--- src/widgets/TasksGrid/ui/TasksGrid.tsx
+++ src/widgets/TasksGrid/ui/TasksGrid.tsx
@@ -5,13 +5,18 @@ import { TaskCard } from '@/entities/task'
 import { selectTasks } from '@/entities/task/model/slice'
 import { DeleteTaskButton } from '@/features/task/delete'
 import { ChangeStatusButton } from '@/features/task/changeStatus'
-import { selectFilter, TasksFilter } from '@/features/task/filter'
+import {
+  MobileTasksFilter,
+  selectFilter,
+  TasksFilter,
+} from '@/features/task/filter'
 import { useMemo } from 'react'
+import { useDeviceQuery } from '@/shared/lib'
 
 export function TasksGrid() {
   const tasks = useSelector(selectTasks)
   const filter = useSelector(selectFilter)
-
+  const { isMobile } = useDeviceQuery()
   const filteredTasks = useMemo(() => {
     if (filter === 'all') return tasks
     return tasks.filter((v) => v.status === filter)
@@ -19,8 +24,8 @@ export function TasksGrid() {
 
   return (
     <Container sx={{ mt: 4 }} maxWidth='xl'>
-      <TasksFilter />
-      <Grid sx={{ mt: 1 }} container spacing={2}>
+      {isMobile ? <MobileTasksFilter /> : <TasksFilter />}
+      <Grid sx={{ mt: 1 }} container spacing={1}>
         {filteredTasks.map((v) => (
           <Grid key={v.id} size={{ xs: 12, lg: 6, xl: 4 }}>
             <TaskCard
