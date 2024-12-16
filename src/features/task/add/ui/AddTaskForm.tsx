f449b5a74acf24484a7aa454d198e6f6ce70c976
domnitskiy.oleg <o.domnitskiy@appomart.com>
2024-12-16 15:24:51 +0400

    Remove unnecessary import of Box component in AddTaskForm

--- src/features/task/add/ui/AddTaskForm.tsx
+++ src/features/task/add/ui/AddTaskForm.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Container, Paper, Stack, TextField } from '@mui/material'
+import { Button, Container, Paper, Stack, TextField } from '@mui/material'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
